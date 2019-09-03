dc188385cdb08b14417db3d6728d0585aa129ea7
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-09-03 10:32:02 +0200

    Upgrade to devon4j 3.1.1 and added support for Java 8-11

--- core/src/main/java/com/devonfw/keywi/general/common/impl/config/ApplicationObjectMapperFactory.java
+++ core/src/main/java/com/devonfw/keywi/general/common/impl/config/ApplicationObjectMapperFactory.java
@@ -2,20 +2,13 @@ package com.devonfw.keywi.general.common.impl.config;
 
 import javax.inject.Named;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.web.csrf.CsrfToken;
 
-import com.devonfw.module.basic.common.api.reference.IdRef;
-import com.devonfw.module.basic.common.api.reference.json.IdRefJsonDeserializer;
-import com.devonfw.module.basic.common.api.reference.json.IdRefJsonSerializer;
 import com.devonfw.module.criteria.common.api.query.LiteralSearchCriteria;
 import com.devonfw.module.criteria.common.api.query.StringSearchCriteria;
 import com.devonfw.module.criteria.common.api.query.json.LiteralSearchCriteriaJsonSerializer;
 import com.devonfw.module.criteria.common.api.query.json.StringSearchCriteriaJsonDeserializer;
 import com.devonfw.module.json.common.base.ObjectMapperFactory;
-import com.devonfw.module.json.common.base.type.PageableJsonDeserializer;
-import com.devonfw.module.json.common.base.type.PageableJsonSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 /**
@@ -33,15 +26,6 @@ public class ApplicationObjectMapperFactory extends ObjectMapperFactory {
     // see https://github.com/devonfw-wiki/devon4j/wiki/guide-json#json-and-inheritance
     SimpleModule module = getExtensionModule();
     module.addAbstractTypeMapping(CsrfToken.class, CsrfTokenImpl.class);
-    // register spring-data Pageable
-    module.addSerializer(Pageable.class, new PageableJsonSerializer());
-    module.addDeserializer(Pageable.class, new PageableJsonDeserializer());
-    // register spring-data Page
-    module.addAbstractTypeMapping(Page.class, JsonPage.class);
-    module.setMixInAnnotation(Page.class, JsonPage.class);
-    // register IdRef mapping
-    module.addSerializer(IdRef.class, new IdRefJsonSerializer());
-    module.addDeserializer(IdRef.class, new IdRefJsonDeserializer());
     // register StringSearchCriteria mapping
     module.addSerializer(LiteralSearchCriteria.class, new LiteralSearchCriteriaJsonSerializer());
     module.addDeserializer(StringSearchCriteria.class, new StringSearchCriteriaJsonDeserializer());
